c3221515edb970e357d64215aedfcd548c128a2b
JosuePerez4 <josuedanielpg@ufps.edu.co>
2026-05-02 18:57:05 -0500

    rikiti

--- src/main/java/com/taller/entrega/EntregaController.java
+++ src/main/java/com/taller/entrega/EntregaController.java
@@ -4,14 +4,12 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/entregas")
-@CrossOrigin(origins = {"http://localhost:5173", "http://127.0.0.1:5173"})
 public class EntregaController {
 
     @Autowired
